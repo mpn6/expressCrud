bb9f6300eb3e82b96b9677e2bffc0ebf6d134b58
Michelle Nunez <mpnunez5@gmail.com>
2021-05-08 18:45:31 -0400

    Set up app.js

--- server/app.js
+++ server/app.js
@@ -1,4 +1,28 @@
-'use strict';
+const express = require('express');
+const exphbs = require('express-handlebars');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// To support URL-encoded bodies
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// To parse cookies from the HTTP Request
+app.use(cookieParser());
+
+app.engine('hbs', exphbs({
+    extname: '.hbs'
+}));
+
+app.set('view engine', 'hbs');
+
+// Our requests hadlers will be implemented here...
+
+app.listen(3000);
+
+
+/*'use strict';
 const express = require('express');
 const open = require("open");
 const bodyParser = require('body-parser');
@@ -35,3 +59,4 @@ app.listen(app.get('port'), function() {
     open("http://localhost:8000");
 
 });
+*/
