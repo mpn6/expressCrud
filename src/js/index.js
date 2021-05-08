97e64bf7e21ed87549f93decc689dfb677dd97b4
Michelle Nunez <mpnunez5@gmail.com>
2021-05-08 18:22:58 -0400

    fixed no-use-before-define errors

--- src/js/index.js
+++ src/js/index.js
@@ -7,26 +7,6 @@ getJSON('', (data) => {
   console.log(data);
 });
 
-getJSON('http://localhost:8000/api/v1/cities',
-  (err, records) => {
-    if (err !== null) {
-      alert(`Something went wrong: ${err}`);
-    } else {
-      const table = document.querySelector('table');
-      const data = Object.keys((records.data[0]));
-      const dataRecords = records.data;
-
-      generateTableHead(table, data);
-      generateTable(table, dataRecords);
-      /*
-      let data = Object.keys(records.data[0]);
-      generateTable(table, records.data); // generate the table first
-      generateTableHead(table, data); // then the head
-      */
-    }
-  });
-ConsoleLogIt('this workedss  in the bundle');
-
 function generateTableHead(table, data) {
   const thead = table.createTHead();
   const row = thead.insertRow();
@@ -50,3 +30,23 @@ function generateTable(table, data) {
     }
   }
 }
+
+getJSON('http://localhost:8000/api/v1/cities',
+    (err, records) => {
+      if (err !== null) {
+        alert(`Something went wrong: ${err}`);
+      } else {
+        const table = document.querySelector('table');
+        const data = Object.keys((records.data[0]));
+        const dataRecords = records.data;
+
+        generateTableHead(table, data);
+        generateTable(table, dataRecords);
+        /*
+        let data = Object.keys(records.data[0]);
+        generateTable(table, records.data); // generate the table first
+        generateTableHead(table, data); // then the head
+        */
+      }
+    });
+ConsoleLogIt('this workedss  in the bundle');
