d0445a076dc546ece86ec5211789df0ea81a82af
Michelle Nunez <mpnunez5@gmail.com>
2021-05-08 18:11:31 -0400

    Fixed multiple indent, prefer-const, prefer-arrow-callback errors

--- src/js/index.js
+++ src/js/index.js
@@ -3,18 +3,18 @@ import ConsoleLogIt from './ConsoleLogIt';
 import getJSON from './getJSON';
 
 TestJS();
-getJSON("", function (data) {
-    console.log(data);
+getJSON('', (data) => {
+  console.log(data);
 });
 
 getJSON('http://localhost:8000/api/v1/cities',
-    function(err, records) {
+  (err, records) => {
         if (err !== null) {
             alert('Something went wrong: ' + err);
         } else {
-            let table = document.querySelector("table");
-            let data = Object.keys((records.data[0]));
-            let dataRecords = records.data;
+            const table = document.querySelector("table");
+            const data = Object.keys((records.data[0]));
+            const dataRecords = records.data;
 
             generateTableHead(table, data);
             generateTable(table, dataRecords);
@@ -26,7 +26,7 @@ getJSON('http://localhost:8000/api/v1/cities',
         }
 
     });
-ConsoleLogIt("this workedss  in the bundle");
+ConsoleLogIt('this workedss  in the bundle');
 
 function generateTableHead(table, data) {
     let thead = table.createTHead();
