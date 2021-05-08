c0072109b3e7134739c70c584ff268e38a064b16
Michelle Nunez <mpnunez5@gmail.com>
2021-05-08 18:24:28 -0400

    fixed indent errors

--- src/js/index.js
+++ src/js/index.js
@@ -32,21 +32,22 @@ function generateTable(table, data) {
 }
 
 getJSON('http://localhost:8000/api/v1/cities',
-    (err, records) => {
-      if (err !== null) {
-        alert(`Something went wrong: ${err}`);
-      } else {
-        const table = document.querySelector('table');
-        const data = Object.keys((records.data[0]));
-        const dataRecords = records.data;
+  (err, records) => {
+    if (err !== null) {
+      alert(`Something went wrong: ${err}`);
+    }
+    else {
+      const table = document.querySelector('table');
+      const data = Object.keys((records.data[0]));
+      const dataRecords = records.data;
 
-        generateTableHead(table, data);
-        generateTable(table, dataRecords);
-        /*
-        let data = Object.keys(records.data[0]);
-        generateTable(table, records.data); // generate the table first
-        generateTableHead(table, data); // then the head
-        */
-      }
-    });
+      generateTableHead(table, data);
+      generateTable(table, dataRecords);
+      /*
+      let data = Object.keys(records.data[0]);
+      generateTable(table, records.data); // generate the table first
+      generateTableHead(table, data); // then the head
+      */
+    }
+  });
 ConsoleLogIt('this workedss  in the bundle');
