2bb2bd0f3d071e96ed3fcf8b7518c8503e9d2663
Michelle Nunez <mpnunez5@gmail.com>
2021-05-08 21:32:04 -0400

    required external modules for auth

---
+++ auth.js
@@ -0,0 +1,20 @@
+// auth.js
+
+/**
+ * Required External Modules
+ */
+const express = require("express");
+const router = express.Router();
+const passport = require("passport");
+const querystring = require("querystring");
+
+require("dotenv").config();
+
+/**
+ * Routes Definitions
+ */
+
+
+/**
+ * Module Exports
+ */
