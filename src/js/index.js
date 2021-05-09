bcd2ded5e4bc8f166e48daae2bacef19e9926557
Michelle Nunez <mpnunez5@gmail.com>
2021-05-09 13:52:49 -0400

    set up the index.js with the app configuration

--- src/js/index.js
+++ src/js/index.js
@@ -1,53 +1,47 @@
-import TestJS from "./TestJs.js";
-import ConsoleLogIt from "./ConsoleLogIt.js";
-import getJSON from "./getJSON";
+/**
+ * Required External Modules
+ */
 
-TestJS();
-getJSON("", function (data) {
-    console.log(data);
-});
+const express = require("express");
+const path = require("path");
 
-getJSON('http://localhost:8000/api/v1/cities',
-    function(err, records) {
-        if (err !== null) {
-            alert('Something went wrong: ' + err);
-        } else {
-            let table = document.querySelector("table");
-            let data = Object.keys((records.data[0]));
-            let dataRecords = records.data;
+const expressSession = require("express-session");
+const passport = require("passport");
+const Auth0Strategy = require("passport-auth0");
 
-            generateTableHead(table, data);
-            generateTable(table, dataRecords);
-            /*
-            let data = Object.keys(records.data[0]);
-            generateTable(table, records.data); // generate the table first
-            generateTableHead(table, data); // then the head
-             */
-        }
+require("dotenv").config();
 
-    });
-ConsoleLogIt("this workedss  in the bundle");
+/**
+ * App Variables
+ */
 
-function generateTableHead(table, data) {
-    let thead = table.createTHead();
-    let row = thead.insertRow();
-    for (let key of data) {
-        let th = document.createElement("th");
-        let text = document.createTextNode(key);
-        th.appendChild(text);
-        row.appendChild(th);
-    }
+const app = express();
+const port = process.env.PORT || "8000";
+
+/**
+ * Session Configuration (New!)
+ */
+
+const session = {
+    secret: process.env.SESSION_SECRET,
+    cookie: {},
+    resave: false,
+    saveUninitialized: false
+};
+
+if (app.get("env") === "production") {
+    // Serve secure cookies, requires HTTPS
+    session.cookie.secure = true;
 }
 
-function generateTable(table, data) {
-    for (let element of data) {
-        let row = table.insertRow();
-        console.log(element);
-        let key;
-        for (key in element) {
-            let cell = row.insertCell();
-            let text = document.createTextNode(element[key]);
-            cell.appendChild(text);
-        }
-    }
-}
+
+
+/**
+ * Passport Configuration (New!)
+ */
+
+
+
+/**
+ *  App Configuration
+ */
