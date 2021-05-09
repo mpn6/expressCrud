92b8cb9f3129be1fa8a3529f328b54e5e8497568
Michelle Nunez <mpnunez5@gmail.com>
2021-05-09 15:14:15 -0400

    get callback definition

--- server/auth.js
+++ server/auth.js
@@ -24,6 +24,26 @@ router.get(
     }
 );
 
+router.get("/callback", (req, res, next) => {
+    passport.authenticate("auth0", (err, user, info) => {
+        if (err) {
+            return next(err);
+        }
+        if (!user) {
+            return res.redirect("/login");
+        }
+        req.logIn(user, (err) => {
+            if (err) {
+                return next(err);
+            }
+            const returnTo = req.session.returnTo;
+            delete req.session.returnTo;
+            res.redirect(returnTo || "/");
+        });
+    })(req, res, next);
+});
+
+
 /**
  * Module Exports
  */
