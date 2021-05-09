b20aebd51910cff8e5e68328d2dd836db09df86d
Michelle Nunez <mpnunez5@gmail.com>
2021-05-09 15:12:58 -0400

    import required modules

--- server/auth.js
+++ server/auth.js
@@ -4,6 +4,13 @@
  * Required External Modules
  */
 
+const express = require("express");
+const router = express.Router();
+const passport = require("passport");
+const querystring = require("querystring");
+
+require("dotenv").config();
+
 /**
  * Routes Definitions
  */
