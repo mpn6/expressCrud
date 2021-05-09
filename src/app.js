87955aa4854da5910a8ad1708939b931b0b8a0e8
Michelle Nunez <mpnunez5@gmail.com>
2021-05-08 20:18:29 -0400

    set up registration

--- src/app.js
+++ src/app.js
@@ -5,6 +5,14 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const crypto = require('crypto');
+
+const getHashedPassword = (password) => {
+    const sha256 = crypto.createHash('sha256');
+    const hash = sha256.update(password).digest('base64');
+    return hash;
+}
+
 // To support URL-encoded bodies
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -19,12 +27,58 @@ app.set('view engine', 'hbs');
 
 // Our requests hadlers will be implemented here...
 
-app.get('/', function (req, res) {
-    res.render('home');
-});
-
 app.get('/register', (req, res) => {
     res.render('register');
+});
+
+const users = [
+    // This user is added to the array to avoid creating a new user on each restart
+    {
+        firstName: 'John',
+        lastName: 'Doe',
+        email: 'johndoe@email.com',
+        // This is the SHA256 hash for value of `password`
+        password: 'XohImNooBHFR0OVvjcYpJ3NgPQ1qq73WKhHvch0VQtg='
+    }
+];
+
+app.post('/register', (req, res) => {
+    const { email, firstName, lastName, password, confirmPassword } = req.body;
+
+    // Check if the password and confirm password fields match
+    if (password === confirmPassword) {
+
+        // Check if user with the same email is also registered
+        if (users.find(user => user.email === email)) {
+
+            res.render('register', {
+                message: 'User already registered.',
+                messageClass: 'alert-danger'
+            });
+
+            return;
+        }
+
+        const hashedPassword = getHashedPassword(password);
+
+        // Store user into the database if you are using one
+        users.push({
+            firstName,
+            lastName,
+            email,
+            password: hashedPassword
+        });
+
+        res.render('login', {
+            message: 'Registration Complete. Please login to continue.',
+            messageClass: 'alert-success'
+        });
+    } else {
+        res.render('register', {
+            message: 'Password does not match.',
+            messageClass: 'alert-danger'
+        });
+    }
 });
 
 
