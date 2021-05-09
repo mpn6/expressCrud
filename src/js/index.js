9457dba333c073a4252683fd401765381d039a3b
Michelle Nunez <mpnunez5@gmail.com>
2021-05-09 14:02:29 -0400

    updated app configuration

--- src/js/index.js
+++ src/js/index.js
@@ -73,3 +73,11 @@ app.use(expressSession(session));
 passport.use(strategy);
 app.use(passport.initialize());
 app.use(passport.session());
+
+passport.serializeUser((user, done) => {
+    done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+    done(null, user);
+});
