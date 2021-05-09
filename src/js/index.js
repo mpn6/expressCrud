7381460d52326efc21c67b11839abd1f157adfdc
Michelle Nunez <mpnunez5@gmail.com>
2021-05-08 21:30:37 -0400

    store and retrieve user data from the session

--- src/js/index.js
+++ src/js/index.js
@@ -75,4 +75,12 @@ passport.use(strategy);
 app.use(passport.initialize());
 app.use(passport.session());
 
+passport.serializeUser((user, done) => {
+    done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+    done(null, user);
+});
+
 // Rest of code...
